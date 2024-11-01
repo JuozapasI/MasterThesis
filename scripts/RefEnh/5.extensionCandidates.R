3daa8ccddf8325f880aa882953f7b659f2ada862
Juozapas <juozapas.ivanauskas@mf.stud.vu.lt>
2024-11-01 16:32:05 +0200

    Fixing code.

--- scripts/RefEnh/5.extensionCandidates.R
+++ scripts/RefEnh/5.extensionCandidates.R
@@ -18,7 +18,11 @@ extension_candidates$cpm = extension_candidates$count/seq_depth*1000000
 # extension_candidates = extension_candidates[extension_candidates$cpm > cpmThreshold, ]
 
 # Make sure that we have unique entries, in the case there are 2 clusters for one gene, leave only the bigger one).
-extension_candidates = extension_candidates[!duplicated(extension_candidates$gene), ]
+extension_candidates = extension_candidates[!duplicated(extension_candidates$closest_gene), ]
+
+# Make nice numbering and rename 'closest_gene' to gene:
+rownames(extension_candidates) <- 1:nrow(extension_candidates)
+colnames(extension_candidates)[6] <- 'gene'
 
 # Save the list:
 write.csv(extension_candidates, "extension_candidates.csv")
@@ -26,6 +30,9 @@ write.csv(extension_candidates, "extension_candidates.csv")
 # Additionally let's create a list of new gene candidates:
 new_genes <- intergenic_clusters[-1*intergenic_clusters$distance > extensionThreshold, ]
 new_genes$cpm = new_genes$count/seq_depth*1000000
+
+# Make nice numbering
+rownames(new_genes) <- 1:nrow(new_genes)
 
 # Save the list:
 write.csv(new_genes, "new_gene_candidates.csv")
