d3520139300f8356b48e8ca2785ff5896b619cdd
Juozapas <juozapas.ivanauskas@mf.stud.vu.lt>
2024-11-04 10:11:41 +0200

    Adding some starsolo script used.

---
+++ scripts/solo/index_RE.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J solo_J
+#SBATCH -o /tmp/Mazutislab-out/Juozapas/Thesis/data/PBMC_10x/index_10x_RE/slurm.log
+#SBATCH --partition Cluster-public
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=14
+#SBATCH --mem=120G
+#SBATCH --time=48:00:00
+
+
+# run STAR
+STAR \
+    --runMode genomeGenerate \
+    --runThreadN 14 \
+    --genomeDir /tmp/Mazutislab-out/Juozapas/Thesis/data/PBMC_10x/index_10x_RE/ \
+    --genomeFastaFiles /tmp/Mazutislab-out/Juozapas/Thesis/data/GRCh38.dna.primary_assembly.fa \
+    --sjdbGTFfile /tmp/Mazutislab-out/Juozapas/Thesis/data/PBMC_10x/RefEnh/RE.gtf
+
