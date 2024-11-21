6508799a158c155af66f0ed2559d5f621d72170c
Juozapas <juozapas.ivanauskas@mf.stud.vu.lt>
2024-11-21 11:28:08 +0200

    Adding file contents.

--- scripts/slurm/make.sh
+++ scripts/slurm/make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -J RE
-#SBATCH -o /tmp/Mazutislab-out/Juozapas/REv3/slurm.log
+#SBATCH -o /tmp/Mazutislab-out/Juozapas/Thesis/slurm_make.log
 #SBATCH --partition Cluster-public
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=14
@@ -8,21 +8,6 @@
 #SBATCH --time=48:00:00
 
 
-# activate conda-env, if the version from env needed
-source activate /home/MazutisLab/software/pkg/miniconda3/envs/RefEnh
-export PATH="/home/MazutisLab/software/pkg/miniconda3/envs/star-J/bin:$PATH"
-    
-Rscript /tmp/Mazutislab-out/Juozapas/Thesis/scripts/RefEnh/main1.R \
-  /tmp/Mazutislab-out/Juozapas/Thesis/data/PBMC_10x/RefEnh/ \
-  /tmp/Mazutislab-out/Juozapas/Thesis/data/genes_10x.gtf \
-  /tmp/Mazutislab-out/Juozapas/Thesis/data/PBMC_10x/solo_output/Aligned.sortedByCoord.out.bam \
-  /tmp/Mazutislab-out/Juozapas/Thesis/data/PBMC_10x/solo_output/Aligned.sortedByCoord.out.bam.bai \
-  /tmp/Mazutislab-out/Juozapas/Thesis/data/GRCh38.dna.primary_assembly.fa \
-  /tmp/Mazutislab-out/Juozapas/Thesis/data/GRCh38.dna.primary_assembly.fa.fai \
-  /tmp/Mazutislab-out/Juozapas/Thesis/data/chr_lenghts.tsv \
-  10000 \
-  182330834 \
-  1000 \
-  5
-
+cd /tmp/Mazutislab-out/Juozapas/Thesis/
+make data/PBMC_10x/solo_intergenic_gencode/Aligned.sortedByCoord.out.bam
 
