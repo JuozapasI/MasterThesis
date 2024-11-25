ac47d0d2c4b4bd995659b4a5ca523d8259cc2793
Juozapas <juozapas.ivanauskas@mf.stud.vu.lt>
2024-11-25 12:06:18 +0200

    Adding bash script for taking unassigned reads.

---
+++ scripts/bash/take_unassigned.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+( samtools view -H $1  && samtools view -F 1024 -F 256 $1 | \
+	grep "GN:Z:-" | \
+	grep -P "\bNH:i:1\b" | \
+	grep -E "CB:Z:[A-Z]{$3}" | \
+	grep -E "UB:Z:[A-Z]{$4)}" ) | \
+	samtools view -h -b - > $2
