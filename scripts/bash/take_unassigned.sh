dccffdcd726e307f85d72e50c8f3b23c900dbb56
Juozapas <juozapas.ivanauskas@mf.stud.vu.lt>
2025-01-03 16:46:36 +0200

    Small fixes.

--- scripts/bash/take_unassigned.sh
+++ scripts/bash/take_unassigned.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-( samtools view -H $1  && samtools view -F 1024 -F 256 $1 | \
+( samtools view -H $1 && samtools view -F 1024 -F 256 $1 | \
 	grep "GN:Z:-" | \
 	grep -P "\bNH:i:1\b" | \
-	grep -E "CB:Z:[A-Z]{$3}" | \
-	grep -E "UB:Z:[A-Z]{$4}" ) | \
+	grep -E "CB:Z:[A-Z_]{$3}" | \
+	grep -E "UB:Z:[A-Z_]{$4}" ) | \
 	# if removing comments, remove ')' above
 	# awk 'BEGIN {FS="\t"; OFS="\t"} {if ($0 ~ /CB:Z:/ && $0 ~ /UB:Z:/) { \
 	# split($0, a, "CB:Z:"); split(a[2], b, " "); cb = b[1]; \
