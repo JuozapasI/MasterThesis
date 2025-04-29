d6dfaa7855de8b2dd4a172de8ea3ef8732a1a6a4
Juozapas <juozapas.ivanauskas@mf.stud.vu.lt>
2025-04-29 15:26:28 +0300

    updated pipeline submission script.

--- scripts/slurm/make.sh
+++ scripts/slurm/make.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #SBATCH -J make-j
-#SBATCH -o /tmp/Mazutislab-out/Juozapas/Thesis/slurm_make.log
+#SBATCH -o /tmp/Mazutislab-out/Juozapas/Thesis/data/datasets/brain_2/slurm_make.log
 #SBATCH --partition Cluster-public
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=14
 #SBATCH --mem=120G
-#SBATCH --time=48:00:00
+#SBATCH --time=72:00:00
 
-source activate /home/MazutisLab/software/pkg/miniconda3/envs/RefEnh
-export PATH="/home/MazutisLab/software/pkg/miniconda3/envs/RefEnh/bin:$PATH"
+source activate /home/MazutisLab/software/pkg/miniconda3/envs/RE
+export PATH="/home/MazutisLab/software/pkg/miniconda3/envs/RE/bin:$PATH"
+
+sample=$1
+umi=$2
+barcode=$3
 
 cd /tmp/Mazutislab-out/Juozapas/Thesis/
-make all
+make ${sample}.dataset umi=${umi} barcode=${barcode}
